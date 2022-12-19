5f9bc758fd1398a5c28115db4ce932681bcd6ccb
Ritvik Saran S <codebyritvik@gmail.com>
2022-12-19 16:46:38 +0530

    productpage

--- ReactJs/ecommerce/frontend/src/screens/Home.js
+++ ReactJs/ecommerce/frontend/src/screens/Home.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -39,6 +40,9 @@ const Home = () => {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>Shoppie</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
