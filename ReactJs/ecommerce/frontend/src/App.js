6d72afb6d3f479e31954319a77876d6acf90db7d
Ritvik Saran S <codebyritvik@gmail.com>
2022-12-19 18:19:39 +0530

    cart functionality

--- ReactJs/ecommerce/frontend/src/App.js
+++ ReactJs/ecommerce/frontend/src/App.js
@@ -1,11 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Product from "./screens/Product";
 import Navbar from "react-bootstrap/Navbar";
+import Badge from "react-bootstrap/Badge";
+import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
+import { useContext } from "react";
+import { Store } from "./Store";
+import Cart from "./screens/Cart";
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
@@ -15,6 +22,16 @@ function App() {
               <LinkContainer to="/">
                 <Navbar.Brand>Shoppie</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
@@ -23,6 +40,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/product/:slug" element={<Product />} />
+              <Route path="/cart" element={<Cart />} />
             </Routes>
           </Container>
         </main>
