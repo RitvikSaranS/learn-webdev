a4b85bd65b146b256429a5e0642d8a297dc9a9db
Ritvik Saran S <codebyritvik@gmail.com>
2022-12-19 14:55:42 +0530

    ecommerce

--- ReactJs/ecommerce/frontend/src/App.js
+++ ReactJs/ecommerce/frontend/src/App.js
@@ -1,20 +1,34 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Product from "./screens/Product";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">Shoppie</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Shoppie</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:slug" element={<Product />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product/:slug" element={<Product />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights are reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
